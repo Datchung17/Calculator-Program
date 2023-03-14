07db8644b3b6c0d58392ff269afa2ebe708e3f5a
Dat Chung <datchung17@gmail.com>
2023-03-14 14:24:49 -0500

    add advance feature to the calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,40 +3,74 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double num1, num2, result;
-        char operator;
 
+        // Get the first number
         System.out.print("Enter first number: ");
-        num1 = input.nextDouble();
+        double num1 = input.nextDouble();
 
-        System.out.print("Enter second number: ");
-        num2 = input.nextDouble();
+        // Get the operation to perform
+        System.out.print("Enter operation (+, -, *, /, %, ^, sqr, sqrt): ");
+        String operation = input.next();
 
-        System.out.print("Enter operator (+, -, *, /): ");
-        operator = input.next().charAt(0);
+        // Get the second number
+        System.out.print("Enter a second number: ");
+        double num2 = input.nextDouble();
 
-        switch(operator) {
-            case '+':
+        double result;
+        switch (operation) {
+            case "+":
                 result = num1 + num2;
                 System.out.println(num1 + " + " + num2 + " = " + result);
                 break;
-            case '-':
+            case "-":
                 result = num1 - num2;
                 System.out.println(num1 + " - " + num2 + " = " + result);
                 break;
-            case '*':
+            case "*":
                 result = num1 * num2;
                 System.out.println(num1 + " * " + num2 + " = " + result);
                 break;
-            case '/':
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero.");
+                    break;
+                }
                 result = num1 / num2;
                 System.out.println(num1 + " / " + num2 + " = " + result);
                 break;
+            case "%":
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero.");
+                    break;
+                }
+                result = num1 % num2;
+                System.out.println(num1 + " % " + num2 + " = " + result);
+                break;
+            case "^":
+                // Get the second number for power
+                System.out.print("Enter power: ");
+                int power = input.nextInt();
+                result = Math.pow(num1, power);
+                System.out.println(num1 + " ^ " + power + " = " + result);
+                break;
+            case "sqr":
+                // Square the first number
+                result = num1 * num1;
+                System.out.println(num1 + " squared = " + result);
+                break;
+            case "sqrt":
+                // Take the square root of the first number
+                if (num1 < 0) {
+                    System.out.println("Cannot take square root of a negative number.");
+                    break;
+                }
+                result = Math.sqrt(num1);
+                System.out.println("Square root of " + num1 + " = " + result);
+                break;
             default:
-                System.out.println("Invalid operator");
+                System.out.println("Invalid operation.");
                 break;
         }
-
         input.close();
     }
 }
